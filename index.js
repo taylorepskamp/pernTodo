1723a322fd7e956d15f213205ec58a1956056a4d
Taylor Epskamp <62791077+taylorepskamp@users.noreply.github.com>
2020-09-24 16:26:20 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,93 +1,109 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const pool = require('./db')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const pool = require("./db");
+const path = require("path");
+const PORT = process.env.PORT || 5000;
 
 //process.env.PORT
 //process.env.NODE_ENV => production or undefined
 
 //middleware
-app.use(cors())
-app.use(express.json()); //req.body
+app.use(cors());
+app.use(express.json()); // => allows us to access the req.body
 
-if(process.env.NODE_ENV === 'production'){
-    //server static content
-    //npm run build
-    app.use(express.static(path.join(__dirname, 'client/build')))
+// app.use(express.static(path.join(__dirname, "client/build")));
+// app.use(express.static("./client/build")); => for demonstration
+
+if (process.env.NODE_ENV === "production") {
+  //server static content
+  //npm run build
+  app.use(express.static(path.join(__dirname, "client/build")));
 }
 
-//Routes//
+console.log(__dirname);
+console.log(path.join(__dirname, "client/build"));
 
-//create a todo
+//ROUTES//
 
-app.post('/todos', async (req,res)=>{
-    try {
-        const {description} = req.body
-        const newTodo = await pool.query("INSERT INTO todo (description) VALUES($1) RETURNING *",
-         [description]
-        );
-      
-        res.json(newTodo.rows[0])
-        
-    } catch (err) {
-        console.error(err.message)
-    }
-})
+//get all Todos
 
-//get all todos
+app.get("/todos", async (req, res) => {
+  try {
+    const allTodos = await pool.query("SELECT * FROM todo");
 
-app.get('/todos', async(req,res) =>{
-    try {
-        const allTodos = await pool.query("SELECT * FROM todo")
-        res.json(allTodos.rows);
-    } catch (err) {
-        console.error(err.message)
-    }
-})
+    res.json(allTodos.rows);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
 
 //get a todo
 
-app.get('/todos/:id', async(req,res) =>{
-    try {
-        const {id} = req.params
-        const todo = await pool.query("SELECT * FROM todo WHERE todo_id = $1", [id])
-        res.json(todo.rows[0])
-    } catch (err) {
-        console.error(err.message)
-    }
-})
+app.get("/todos/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const todo = await pool.query("SELECT * FROM todo WHERE todo_id = $1", [
+      id,
+    ]);
+    res.json(todo.rows[0]);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
+
+//create a todo
+
+app.post("/todos", async (req, res) => {
+  try {
+    console.log(req.body);
+    const { description } = req.body;
+    const newTodo = await pool.query(
+      "INSERT INTO todo (description) VALUES ($1) RETURNING *",
+      [description]
+    );
+
+    res.json(newTodo.rows[0]);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
 
 //update a todo
 
-app.put('/todos/:id', async (req,res) => {
-    try {
-        const {id} = req.params;
-        const {description} = req.body;
-        const updateTodo = await pool.query('UPDATE todo SET description = $1 WHERE todo_id= $2', [description, id])
-        res.json("Todo was updated")
-    } catch (err) {
-        console.log(err.message)
-    }
-})
+app.put("/todos/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { description } = req.body;
+    const updateTodo = await pool.query(
+      "UPDATE todo SET description = $1 WHERE todo_id = $2",
+      [description, id]
+    );
+
+    res.json("Todo was updated");
+  } catch (err) {
+    console.error(err.message);
+  }
+});
 
 //delete a todo
 
-app.delete('/todos/:id', async (req,res) =>{
-    try {
-        const {id} = req.params;
-        const deleteTodo = await pool.query('DELETE FROM todo WHERE todo_id = $1',[id])
-        res.json('Todo was deleted')
-    } catch (err) {
-        console.error(err.message)
-    }
-})
+app.delete("/todos/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deleteTodo = await pool.query("DELETE FROM todo WHERE todo_id = $1", [
+      id,
+    ]);
+    res.json("Todo was deleted");
+  } catch (err) {
+    console.error(err.message);
+  }
+});
 
-app.get("*", (req,res) =>{
-    res.sendFile(path.join(__dirname,"client/build/index.html"))
-})
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client/build/index.html"));
+});
 
 app.listen(PORT, () => {
-    console.log(`server has started on port ${PORT}`)
-})
+  console.log(`Server is starting on port ${PORT}`);
+});
